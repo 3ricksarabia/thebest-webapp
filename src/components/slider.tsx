4f98a805c4b65f925f8cb138ac308a673b6f79dd
Erick Sarabia <erick@clappy.io>
2023-05-07 18:52:28 -0600

    feat(players): update 00s players data

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -8,21 +8,21 @@ const Slider = () => {
         {players.map((player, index) => (
           <div key={index} id={`card-${index}`} className={styles.card}>
             <div className={styles.info}>
-              <h3 className={styles.name}>{player}</h3>
-              <p className={styles.team}>Liverpool FC</p>
+              <h3 className={styles.name}>{player.name}</h3>
+              <p className={styles.team}>{player.team}</p>
               <hr />
               <section className={styles.details}>
                 <div className={styles.box}>
                   <h4 className={styles.title}>Age</h4>
-                  <h5 className={styles.value}>31</h5>
+                  <h5 className={styles.value}>{player.age}</h5>
                 </div>
                 <div className={styles.box}>
                   <h4 className={styles.title}>Position</h4>
-                  <h5 className={styles.value}>RW</h5>
+                  <h5 className={styles.value}>{player.position}</h5>
                 </div>
                 <div className={styles.box}>
-                  <h4 className={styles.title}>Trophies</h4>
-                  <h5 className={styles.value}>6</h5>
+                  <h4 className={styles.title}>Nationality</h4>
+                  <h5 className={styles.value}>{player.nationality}</h5>
                 </div>
               </section>
             </div>
